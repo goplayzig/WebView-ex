ab956cadada56aa27c009d1c635e17d527111239
goplayzig <9909inin>
2016-11-14 00:48:51 +0900

    주석 추가

--- webviewex/ViewController.swift
+++ webviewex/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myWebView: UIWebView! //웹뷰
+    let stringURL = "https://www.google.co.kr" //URL 을 나타내는 문자열로  URL 객체 만듬
+
+    
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let Stringurl = URL(string : stringURL)   { //지정한 문자열이 URL 형식인 지 확인 (nil 이 아닌지 확인)
+        
+        
+        let need = URLRequest(url: Stringurl) //Url request
+        
+        myWebView.loadRequest(need) //웹뷰 요청
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
